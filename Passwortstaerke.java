7a2c31d0948846214ff1fc99d445899e03b490e3
MLGLoL09 <153075796+MLGLoL09@users.noreply.github.com>
2024-05-16 15:26:34 +0200

    woerterZaehlen_Bearbeitung

--- Passwortstaerke.java
+++ Passwortstaerke.java
@@ -1,5 +1,37 @@
 public class Passwortstaerke {
-    public static void main(String[] args) {
+    public static int zeichenZaehlen(String text) {
+        int length = text.length();
+        return length;
+    }
+
+    public static int zeichenZaehlenSonderzeichen(String text) {
+        char zeichen;
+        int character = 0;
+        for (int i = 0; i < text.length(); i++) {
+            zeichen = text.charAt(i);
+            if (!Character.isLetterOrDigit(zeichen)) {
+                character++;
+            }
+        }
         
+        return character;
+    }
+    
+
+    public static int woerterZaehlen(String text) {
+        char zeichen;
+        int words = 0;
+        for (int i = 0; i < text.length(); i++) {
+            zeichen = text.charAt(i);
+            if (!Character.isLetterOrDigit(zeichen)) {
+                words++;
+            }
+        }
+        return words;
     }
 }
+/*
+    public static int passwortstaerke(String text) {
+        return;
+    }
+}*/
---
+++ PasswortstaerkeTest.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class PasswortstaerkeTest {
+    public static void main(String[] args) {
+        String password = JOptionPane.showInputDialog(null, "Geben Sie hier Ihr Passwort ein:");
+        int passwordLength = Passwortstaerke.zeichenZaehlen(password);
+        int character = Passwortstaerke.zeichenZaehlenSonderzeichen(password);
+        int words = Passwortstaerke.woerterZaehlen(password);
+        JOptionPane.showMessageDialog(null, "Anzahl der Zeichen: " + passwordLength + "\nAnzahl der Sonderzeichen: " + character + "\nAnzahl der Woerter: " + words);
+    }
+}
